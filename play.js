44da6eb03d144c60fce8f45ed4d22a2e03afea98
Karina Song <ksysong@gmail.com>
2021-03-10 22:51:25 +0000

    refactored play.js and created client file

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require('net');
-
-//establishes connection with the game server
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.2.15', 
-    port: 50541
-  });
-
-  //interpret incoming data as test
-  conn.setEncoding('utf8');
-  conn.on('data', function() {
-    console.log("Server says: you ded cuz you idled");
-  })
-  
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting...');
 connect();
