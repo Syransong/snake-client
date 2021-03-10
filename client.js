33b0e53c08d168d072bca9ef159a39091d84300d
Karina Song <ksysong@gmail.com>
2021-03-10 23:15:16 +0000

    added and commented out  move commands

--- client.js
+++ client.js
@@ -11,11 +11,14 @@ const connect = function() {
   //interpret incoming data as test
   conn.setEncoding('utf8');
 
-  conn.on('connect', function() {
+  conn.on('connect', function(data) {
     console.log("You have successfully connected to the server!");
     conn.write("Name: SYS");
+    // setInterval(function () {
+    //   conn.write("Move: up");
+    // }, 50);  
   });
-  
+
   conn.on('data', function(data) {
     console.log("Server says: you ded cuz you idled");
   });
