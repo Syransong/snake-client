f302e970ac1577645fe84ee7f79db7a98ae4cbb1
Karina Song <ksysong@gmail.com>
2021-03-11 03:32:26 +0000

    added movement capability using wasd keys

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP Connection object
+let connection;
+
 // Setup Input Interface 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,10 +13,23 @@ const setupInput = function() {
   return stdin;
 };
 
+
 const handleUserInput = function(input) {
   if (input ==='\u0003') {
     process.exit();
-  }
+  };
+  if (input === 'w') {
+    connection.write('Move: up');
+  };
+  if (input === 's') {
+    connection.write('Move: down');
+  };
+  if (input === 'a') {
+    connection.write('Move: left');
+  };
+  if (input === 'd') {
+    connection.write('Move: right');
+  };
 };
 
 module.exports = { setupInput };
